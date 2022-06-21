1be94262002e9b1fffd837a41fcb924ef547ca19
gustavorafaeldev <105467235+gustavorafaeldev@users.noreply.github.com>
2022-06-21 19:51:05 -0300

    validação formulário

--- script.js
+++ script.js
@@ -5,14 +5,15 @@ let b7validator = {
 
         let inputs = form.querySelectorAll('input');
 
+        b7validator.clearErrors();
+
         for(let i=0; i<inputs.length;i++) {
             let input = inputs[i];
             let check = b7validator.checkInput(input);
 
             if(check!== true) {
                 send = false;
-                console.log(check);
-                //exibir o erro
+                b7validator.showError(input, check);
             }
         }
 
@@ -20,10 +21,12 @@ let b7validator = {
             form.submit();
         }
     },
+
     checkInput:(input) => {
         let rules = input.getAttribute('data-rules');
         if(rules !== null) {
-            rules.split('|');
+            rules = rules.split('|');
+
             for(let k in rules) {
                 let rDetails = rules[k].split('=');
 
@@ -34,12 +37,46 @@ let b7validator = {
                         }
                     break;
                     case 'min':
+                        if(input.value.length < rDetails[1]) {
+                            return 'Campo tem que ter pelo menos '+ rDetails[1]+ ' caracteres';
+                        }
+                    break;
+                    case 'email':
+                        if(input.value != '') {
+                            let regex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
+                            if(!regex.test(input.value.toLowerCase())) {
+                                return 'Campo de email inválido';
+                            }
+                        }
                     break;
                 }
             }
         }
         return true;
+    },
+    showError:(input, error)=> {
+        input.style.borderColor = '#FF0000';
+
+        let errorElement = document.createElement('div');
+        errorElement.classList.add('error');
+        errorElement.innerHTML = error;
+
+        input.parentElement.insertBefore(errorElement, input.ElementSibling);
+    },
+
+    clearErrors: ()=> {
+        let inputs = form.querySelectorAll('input');
+
+        for(let i=0;i<inputs.length;i++) {
+            inputs[i].style = '';
+        }
+
+        let errorElements = document.querySelectorAll('.error');
+
+        for(let i=0;i<errorElements.length;i++) {
+            errorElements[i].remove();
+        }
     }
 };
 
